54d1ef5115d9c458b4138efe7a289bdc33eab1c9
Raffay Syed <raffayhusayn@gmail.com>
2022-07-04 04:22:13 -0400

    Add code to finish the Card along with styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,26 @@
 import "../styles/Card.css";
+import locationPin from "../images/location-pin.png";
 const newyork =
   "https://www.travelandleisure.com/thmb/gPCyzXAnVMK116tKpy03OWXtEZI=/2000x0/filters:no_upscale():max_bytes(150000):strip_icc():gifv():format(webp)/new-york-city-evening-NYCTG0221-52492d6ccab44f328a1c89f41ac02aea.jpg";
 function Card() {
   return (
     <div className="card">
       <img className="card-image" src={newyork} alt="newyork" />
-      <div className="card-text">
+      <div className="card-info">
+        <div className="location">
+          <img src={locationPin} alt="location" className="location-pin" />
+          <p className="location-country">IRAN</p>
+          <p className="location-link">View on Google Map</p>
+        </div>
         <h1 className="card-title">Card heading</h1>
-        <p className="card-desc">card description</p>
+        <p className="card-date">05 Dec, 2016 - 15 Oct, 2017</p>
+        <p className="card-desc">
+          card ddescription p pedescriptioi afjadk disafd dsfa adfk dafjil
+          dfasfndesc rip tionde scrip tiondesc riptionde script iondescr
+          iptionscription asfka sadkfjla afdklasdjflka afsdkjk dfkj kdf
+          kafjlasdjflas asdfkasjfkasdjfkalf asl fl klajsfklajds l klj fklajdfk k
+          k flakdsjfklasdj lfkjasd
+        </p>
       </div>
     </div>
   );
